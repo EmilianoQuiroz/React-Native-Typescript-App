0f95d27670d852a9cc65e27e85338f17f61c5a9b
Emiliano Quiroz <santiagoquiroz.dev@gmail.com>
2023-02-18 01:48:33 -0300

    StyleSheet

--- src/screens/ContadorScreen.tsx
+++ src/screens/ContadorScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Button, Touchable, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import { useState } from 'react';
 
 export const ContadorScreen = () => {
@@ -7,25 +7,15 @@ export const ContadorScreen = () => {
   const [contador, setContador] = useState(0);
 
   return (
-    <View style={{
-      flex: 1,
-      justifyContent: 'center'
-    }}>
-        <Text style={{
-          textAlign: 'center',
-          fontSize: 40,
-          top: -25
-        }}>
+    <View style={styles.container}>
+        <Text style={styles.title}>
             Contador { contador }
         </Text>
 
         <TouchableOpacity
         onPress={ () => setContador(contador + 1) }
         >
-          <View style={{
-            backgroundColor: 'red',
-            borderRadius:100
-          }}>
+          <View style={styles.botonIncrementar}>
             <Text>
               +1
             </Text>
@@ -35,3 +25,20 @@ export const ContadorScreen = () => {
     </View>
   )
 }
+
+//El StyleSheet por lo general siempre se coloca al final
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center'
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 40,
+    top: -25 
+  },
+  botonIncrementar:{
+    backgroundColor: 'red',
+    borderRadius:100
+  }
+})
