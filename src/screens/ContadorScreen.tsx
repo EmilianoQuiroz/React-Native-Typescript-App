01bb4bed4ac234e8b89be4f685c0b2c627a399c8
Emiliano Quiroz <santiagoquiroz.dev@gmail.com>
2023-02-18 02:09:01 -0300

    Boton de resta

--- src/screens/ContadorScreen.tsx
+++ src/screens/ContadorScreen.tsx
@@ -12,16 +12,29 @@ export const ContadorScreen = () => {
             Contador { contador }
         </Text>
 
+        {/* Boton de suma */}
         <TouchableOpacity
+        style= {styles.fabLocationBR}
         onPress={ () => setContador(contador + 1) }
         >
-          <View style={styles.botonIncrementar}>
-            <Text>
+          <View style={styles.fab}>
+            <Text style={styles.fabText}>
               +1
             </Text>
           </View>
         </TouchableOpacity>
 
+        {/* Boton de resta */}
+        <TouchableOpacity
+        style= {styles.fabLocationBL}
+        onPress={ () => setContador(contador - 1) }
+        >
+          <View style={styles.fab}>
+            <Text style={styles.fabText}>
+              -1
+            </Text>
+          </View>
+        </TouchableOpacity>
     </View>
   )
 }
@@ -37,8 +50,31 @@ const styles = StyleSheet.create({
     fontSize: 40,
     top: -25 
   },
-  botonIncrementar:{
-    backgroundColor: 'red',
-    borderRadius:100
+  /* Locacion del boton flotante al borde inferior derecho */
+  fabLocationBR: {
+    position: 'absolute',
+    bottom: 25,
+    right:25
+  },
+  /* Locacion de Boton Flotante de resta al borde inferior izquierdo */
+  fabLocationBL: {
+    position: 'absolute',
+    bottom: 25,
+    left:25,
+  },
+  /* Flutting Action Bottom: Boton flotante */
+  fab: {
+    backgroundColor: '#5856D6',
+    width: 60,
+    height: 60,
+    borderRadius:100,
+    justifyContent: 'center'
+  },
+  /* Estilos del texto del boton flotante */
+  fabText: {
+    color: 'white',
+    fontSize: 25,
+    fontWeight: 'bold',
+    alignSelf: 'center'
   }
 })
